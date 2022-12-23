b4cdd8eb2b87e6a9b214b700122969fdee300841
Toin <93148435+Alena-Oleynichenko@users.noreply.github.com>
2022-12-24 01:34:59 +0400

    Update main.py

--- лаба1/main.py
+++ лаба1/main.py
@@ -5,9 +5,9 @@ pre_matrix1 = []
 pre_matrix2 = []
 data1 = []
 data2 = []
-data1 = np.genfromtxt('C:\\Users\\matro\\source\\repos\\Паралельная прога лаба1\\Парал Прог л1\\data_1.txt',
+data1 = np.genfromtxt('C:\\Users\\alena\\source\\repos\\пп1\\matrix1.txt',
                       delimiter=" ", dtype=int)
-data2 = np.genfromtxt('C:\\Users\\matro\\source\\repos\\Паралельная прога лаба1\\Парал Прог л1\\data_2.txt',
+data2 = np.genfromtxt('C:\\Users\\alena\\source\\repos\\пп1\\matrix2.txt',
                       delimiter=" ", dtype=int)
 size = 100
 for elem in data1:
@@ -17,7 +17,7 @@ for elem in data2:
 matrix1 = np.matrix(pre_matrix1, dtype=int)
 matrix2 = np.matrix(pre_matrix2, dtype=int)
 matrix_res_py = np.dot(matrix1, matrix2)
-data_res = np.genfromtxt('C:\\Users\\matro\\source\\repos\\Паралельная прога лаба1\\Парал Прог л1\\data_res.txt',
+data_res = np.genfromtxt('C:\\Users\\alena\\source\\repos\\пп1\\matrix3.txt',
                       delimiter=" ", dtype=int)
 pre_matrix_res_cpp = []
 for elem in data_res:
@@ -26,40 +26,4 @@ matrix_res_cpp = np.matrix(pre_matrix_res_cpp, dtype=int)
 if (np.array_equal(matrix_res_py, matrix_res_cpp)):
     print("Nice, equal")
 else:
-    print("Nope, you dump proger")'''
-import numpy as np
-import operator
-import re
-
-pre_matrix1 = []
-pre_matrix2 = []
-data1 = []
-data2 = []
-
-data1 = np.genfromtxt('C:\\Users\\tsvet\\source\\repos\\pplab1\\pplab1\\M1.txt', delimiter=" ", dtype=int)
-data2 = np.genfromtxt('C:\\Users\\tsvet\\source\\repos\\pplab1\\pplab1\\M2.txt',delimiter=" ", dtype=int)
-
-#size = 100
-
-for elem in data1:
-    pre_matrix1.append(elem)
-
-for elem in data2:
-    pre_matrix2.append(elem)
-
-matrix1 = np.matrix(pre_matrix1, dtype=int)
-matrix2 = np.matrix(pre_matrix2, dtype=int)
-
-matrix_res_py = np.dot(matrix1, matrix2)
-
-data_res = np.genfromtxt('C:\\Users\\tsvet\\source\\repos\\pplab1\\pplab1\\M3.txt', delimiter=" ", dtype=int)
-pre_matrix_res_cpp = []
-for elem in data_res:
-    pre_matrix_res_cpp.append(elem)
-
-matrix_res_cpp = np.matrix(pre_matrix_res_cpp, dtype=int)
-
-if (np.array_equal(matrix_res_py, matrix_res_cpp)):
-    print("GREAT JOB")
-else:
-    print("You suck, go practice your programming skills instead of genshin")
+    print("Nope, you dump proger")
